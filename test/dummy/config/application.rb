d207ecb3b44bc3f469c1db12c96b58cabd4117b2
Almir Sarajčić <almir.sarajcic@icloud.com>
2017-02-12 15:13:42 +0100

    Comment out unused frameworks

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,6 +1,15 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+# require "active_model/railtie"
+# require "active_job/railtie"
+# require "active_record/railtie"
+require "action_controller/railtie"
+# require "action_mailer/railtie"
+require "action_view/railtie"
+# require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
 require "google_analytics_rails"
@@ -12,4 +21,3 @@ module Dummy
     # -- all .rb files in that directory are automatically loaded.
   end
 end
-
